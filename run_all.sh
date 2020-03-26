8e22654a92f393933b0aed25d914c7a3a6a33af2
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2020-03-26 00:09:57 -0500

    add runner script

---
+++ run_all.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+root -b -q ToyDisc.C+\(0,1e18\) 
+root -b -q ToyDisc.C+\(0,3.16e17\) 
+root -b -q ToyDisc.C+\(0,1e17\) 
+root -b -q ToyDisc.C+\(0,3.16e16\) 
+root -b -q ToyDisc.C+\(0,1e16\) 
+
+root -b -q ToyDisc.C+\(1,1e18\) 
+root -b -q ToyDisc.C+\(1,3.16e17\) 
+root -b -q ToyDisc.C+\(1,1e17\) 
+root -b -q ToyDisc.C+\(1,3.16e16\) 
+root -b -q ToyDisc.C+\(1,1e16\) 
+
